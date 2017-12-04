a088631e20a8248245523c4878681ff97a464134
NoahLE <noah@noahle.com>
2017-12-04 15:16:45 -0800

    done with tutorial 2 of rabbitmq

---
+++ worker.py
@@ -0,0 +1,25 @@
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello', durable=True)
+
+print(" [*] Waiting for messages. To exit press Ctrl + C")
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % (body,))
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    # A useful command for checking queues if ack is forgotten:
+    # rabbitmqctl.bat list_queues name messages_ready messages_unacknowledged
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='hello')
+
+channel.start_consuming()
