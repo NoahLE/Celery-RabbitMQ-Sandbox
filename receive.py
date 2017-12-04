2acd415dade7092177857e2a7093e2604737f7c0
NoahLE <noah@noahle.com>
2017-12-04 13:27:50 -0800

    done with part 1 of the rabbitmq tutorial

--- receive.py
+++ receive.py
@@ -1,7 +1,19 @@
 import pika
 
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+print(" [*] Waiting for messages. To exit press Ctrl + C")
+
+channel.start_consuming()
